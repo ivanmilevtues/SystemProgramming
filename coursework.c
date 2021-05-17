d79117ac4f6db48f8c32f309c3241d378062e9af
Ivan Milev <ivanmilevtues@gmail.com>
2021-05-17 10:35:18 +0300

    Added benchmark logic, which will time the exec time for an alogrithm

--- coursework.c
+++ coursework.c
@@ -2,20 +2,23 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#include "sorting.h"
 
 #define CHUNK_SIZE 10
-
  
 int load_array(char * filename, int ** array);
 
  
+void benchmark_sort(void (*f)(int *, int), int * array, int array_size);
+
 int main() {
 	int * array;
-	int size = load_array("test_data_1", &array);
-	int i;
-	for(i = 0; i < size; i++) {
-		printf("%d\n", array[i]);
-	}
+	int size = load_array("test_data_5", &array);
+	
+	benchmark_sort(&bubble_sort, array, size);
+
 	return 0;
 }
  
@@ -23,16 +26,29 @@ int load_array(char * filename, int ** result_array) {
 	FILE * fd = fopen(filename, "r");
 	int allocated_size = CHUNK_SIZE;
 	int size = 0;
-	
+
 	int * array = malloc(sizeof(int) * allocated_size);
 
-	while(fscanf(fd, "%d", array + size) >= 0) {
-		if (++size > allocated_size) {
+	while (fscanf(fd, "%d", array + size) >= 0) {
+		if (++size > allocated_size - 1) {
 			allocated_size += CHUNK_SIZE;
 			array = realloc(array, sizeof(int) * allocated_size);
 		}
 
 	}
 	*(result_array) = array;
+
+	fclose(fd);
 	return size;
+}
+
+
+void benchmark_sort(void (*sorting_algo) (int *, int), int * array, int array_size) {
+	time_t start, end;
+	start = time(NULL);
+	(*sorting_algo)(array, array_size);
+	end = time(NULL);
+
+	printf("%ld, %ld, %ld\n", start, end, end - start);
+	free(array);
 }
---
+++ sorting.h
@@ -0,0 +1,126 @@
+void swap(int * a, int * b)  {
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+
+void bubble_sort(int * array, int size) {
+	int i, j;
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size - i; j++) {
+			if(array[i] > array[j]) {
+				swap(array + i, array + j);
+			}
+		}
+	}
+}
+
+
+void quick_sort_helper(int * array, int start, int end) {
+	int i, j, pivot, temp;
+	if(start < end) {
+		pivot = start;
+		i = start;
+		j = end;
+
+		while (i < j) {
+			while (array[i] <= array[pivot] && i < end) {
+				i++;
+			}
+
+			while (array[j] > array[pivot]) {
+				j--;
+			}
+
+			if (i < j) {
+				swap(array + i, array + j);
+			}
+		}
+	}
+
+	temp = array[pivot];
+	array[pivot] = array[j];
+	array[j] = temp;
+
+	quick_sort_helper(array, start, j - 1);
+	quick_sort_helper(array, j + 1, end);
+}
+
+
+void quick_sort(int  * array, int size) {
+	quick_sort_helper(array, 0, size);
+}
+
+
+void selection_sort(int * array, int size) {
+	int i, j, min_indx;
+
+	for (i = 0; i < size - 1; i++) {
+		min_indx = i;
+		for (j = i + 1; j < size; j++) {
+			min_indx = j;
+		}
+
+		swap(array + min_indx, array + i);
+	}
+}
+
+
+void heapify(int * arr, int n, int i ) {
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+
+	if (l < n && arr[l] > arr[largest]) {
+		largest = l;
+	}
+
+	if (r < n && arr[r] > arr[largest]) {
+		largest = r;
+	}
+
+	if (largest != i) {
+		swap(arr + i, arr + largest);
+
+		heapify(arr, n, largest);
+	}
+}
+
+
+void heap_sort(int * array, int size) {
+	int i;
+
+	for (i = size / 2; i >= 0; i--) {
+		heapify(array, size, i);
+	}
+
+	for (i = size - 1; i > 0; i--) {
+		swap(array, array + i);
+
+		heapify(array, i , 0);
+	}
+}
+
+
+void stable_selection_sort(int * array, int size) {
+	int i, j, min, key;
+
+	for(i = 0; i < size - 1; i++) {
+		min = i;
+		
+		for(j = i + 1; j < size; j++) {
+			if (array[min] > array[j]) {
+				min = j;
+			}
+		}
+
+		key = array[min];
+		while(min > i) {
+			array[min] = array[min - 1];
+			min--;
+		}
+
+		array[i] = key;
+	}
+}
